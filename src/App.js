0ff3794a8ead9c30e7a2a7e8583d034421c79ae2
Marcus Edidiong Cletus <edi_mars@outlook.com>
2023-04-13 09:39:20 -0700

    commit all changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Header from './components/Header';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import ForgotPassword from './pages/ForgotPassword';
+import Offers from './pages/Offers';
 
 function App() {
   return (
     <>
-      <h1 className="bg-red-500">Hello World</h1>
+      <Router>
+        <Header />
+           <Routes>
+             <Route path="/"element={<Home />} />
+             <Route path="/profile"element={<Profile />} />
+             <Route path="/sign-in"element={<SignIn />} />
+             <Route path="/sign-up"element={<SignUp />} />
+             <Route path="/forgot-password"element={<ForgotPassword />} />
+             <Route path="/offers"element={<Offers/>} />
+           </Routes>  
+      </Router> 
     </>
   );
 }
