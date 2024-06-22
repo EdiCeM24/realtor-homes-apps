4ff40b2b19175757c79616c3074a8458cb28360e
Marcus Edidiong Cletus <edi_mars@outlook.com>
2024-06-22 06:13:04 +0100

    Install all Changes Modified.

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,33 @@
+import { useState } from 'react';
 
 
 export default function SignIn() {
+  const [formData, setFormdata] = useState({
+    email: "",
+    password: "",
+  });
+  const { email, password } = formData;
+  function onChange(e) {
+    // console.log(e.target.value);
+
+  }
   return (
-    <div>Sign In</div>
+    <section>
+        <h1 className="text-3xl text-center text-rose-300 mt-6 font-bold">Sign In</h1>
+        
+        <div className="flex justify-center flex-wrap items-center px-6 py-12 max-w-6xl mx-auto">
+          <div className='w-full md:w-[67%] lg:w-[40%] mb-12 md:mb-6'>
+            <img src='https://media.istockphoto.com/id/882354884/photo/locking-door-with-a-key.jpg?s=1024x1024&w=is&k=20&c=wbzPywhw-6E61A4KM_MwPlyQocgVospvZUQZP4trMdc=' alt='Key' className='w-full rounded-2xl'/>
+          </div>
+
+          <div className='w-ful md:w-[67%] lg:w-[40%] lg:ml-20'>
+            <form>
+              <input className='w-full' type='email' id='email' value={email} onChange={onChange}/>
+
+              <input type='password' id='' className='w-full ' value={password} />
+            </form>
+          </div>
+        </div>
+    </section>
   )
 }
